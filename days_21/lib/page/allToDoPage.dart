0f83d512486c515d05ada87eba272b0b4e99ffff
damengzai <damengzai@sohu.com>
2022-11-10 17:43:41 +0800

    俗语

--- days_21/lib/page/allToDoPage.dart
+++ days_21/lib/page/allToDoPage.dart
@@ -1,11 +1,20 @@
+import 'dart:math';
+
 import 'package:days_21/view/flipCard/flipCard.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import '../control/toDoListControl.dart';
 import '../view/toDoItemView.dart';
+import '../constant/constants.dart';
 
 class AllToDoPage extends StatelessWidget {
   ToDoListControl toDoListControl = Get.find();
+  
+  String getSY() {
+    int length = sy.length;
+    int i = Random().nextInt(length);
+    return sy[i];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +29,10 @@ class AllToDoPage extends StatelessWidget {
                 key: Key('i' + index.toString()),
                 toDo: toDoListControl.toDoList[index],
               ),
-              back: Text('back', key: Key('back_i_$index')),
+              back: Padding(
+                padding: const EdgeInsets.only(left: 4, right: 4),
+                child: Text(getSY(), key: Key('back_i_$index')),
+              ),
             ); // Text(toDoListControl.toDoList[index].name);
           },
         ),
